2e20e319b067891bb3e9e71ec671f2f5d105735b
ThakareSakshi <sakshithakare121@gmail.com>
2023-12-17 20:28:05 +0530

    updated alt

--- src/components/main/carasoul.js
+++ src/components/main/carasoul.js
@@ -1,7 +1,7 @@
 const Carasoul=()=>{
      return(
        <div class="carasoul">
-         <img src="https://templatedemo.website.com/remote/imageProxy.cfc?method=viewLibraryImage&d=familywellness&s=MEdNNExVRU1VOEJUTDsxUzI8TldKIFAgIAo%3D&w=940-425" />
+         <img src="https://templatedemo.website.com/remote/imageProxy.cfc?method=viewLibraryImage&d=familywellness&s=MEdNNExVRU1VOEJUTDsxUzI8TldKIFAgIAo%3D&w=940-425" alt="img"/>
     
        </div>
      )
